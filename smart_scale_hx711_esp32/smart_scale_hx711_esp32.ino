04098a33e41c56ac7bd3c4dbfc82faab982f2125
Ivan Tolkachev <i.tolkachev@ip-192-168-1-74.eu-west-1.compute.internal>
2021-06-27 18:48:44 +0300

    initial scale logic

---
+++ smart_scale_hx711_esp32/smart_scale_hx711_esp32.ino
@@ -0,0 +1,114 @@
+#include <HX711.h>
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+BLEAdvertising* pAdvertising = NULL;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+int value = 0;
+
+// Сайт для генерирования UUID:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "f5ff08da-50b0-4a3e-b5e8-83509e584475"
+#define CHARACTERISTIC_UUID "25dbb242-e59b-452c-9a04-c37bdb92e00a"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 33;
+const int LOADCELL_SCK_PIN = 32;
+ 
+HX711 scale;
+
+void set_advertising() {
+  BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
+  std::string ass_data = "lolkek";
+  oAdvertisementData.setServiceData(BLEUUID(SERVICE_UUID), ass_data);
+  pAdvertising->setAdvertisementData(oAdvertisementData);
+}
+
+void setup() {
+  Serial.begin(115200);
+  
+  // HX711 
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  // BLE device
+  BLEDevice::init("MyESP32"); // создаем BLE-устройство:
+
+  pServer = BLEDevice::createServer(); // Создаем BLE-сервер
+  pServer->setCallbacks(new MyServerCallbacks()); // Callback на подключение
+
+  BLEService *pService = pServer->createService(SERVICE_UUID); // Создаем BLE-сервис
+  
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ   |
+      BLECharacteristic::PROPERTY_WRITE  |
+      BLECharacteristic::PROPERTY_NOTIFY |
+      BLECharacteristic::PROPERTY_INDICATE
+  ); // Создаем BLE-характеристику
+
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  pCharacteristic->addDescriptor(new BLE2902()); // создаем BLE-дескриптор
+
+  pService->start(); // запускаем сервис
+  
+  pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(BLEUUID(SERVICE_UUID));
+  set_advertising(); 
+  pAdvertising->start(); // запускаем оповещения (advertising)
+  Serial.println("Waiting a client connection to notify...");  //  "Ждем подключения клиента, чтобы отправить ему уведомление..."
+
+}
+ 
+void loop() {
+
+  if (scale.is_ready()) {
+    value = scale.read();
+    if (deviceConnected) {  
+      char txString[13];
+      dtostrf(value, 10, 3, txString);
+      pCharacteristic->setValue(txString);
+      pCharacteristic->notify();
+      //pCharacteristic->indicate();
+      Serial.print("HX711 reading: ");
+      Serial.println(value);
+      Serial.printf("*** NOTIFY: %d ***\n", value);
+    } else {
+      Serial.println("BLE device not found.");
+    }
+  } else {
+    Serial.println("HX711 not found.");
+  }
+
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+      delay(500); // give the bluetooth stack the chance to get things ready
+      pServer->startAdvertising(); // restart advertising
+      Serial.println("start advertising");
+      oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+      // do stuff here on connecting
+      oldDeviceConnected = deviceConnected;
+  }
+  delay(1000);
+}
